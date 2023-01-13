244becfee9e1efd0ce821ff3e604028a5be03c43
Philippe Simonet <philippe.simonet@swisscom.com>
2023-01-13 13:53:33 +0100

    save exe without version name

--- build-windows.sh
+++ build-windows.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 export `./setversion.sh`
 echo $VERSION
-pkg -C gzip --target node16-win-x64 -o dist/x2sql-$VERSION.exe src/x2sql.js
+# pkg -C gzip --target node16-win-x64 -o dist/x2sql-$VERSION.exe src/x2sql.js
+pkg -C gzip --target node16-win-x64 -o dist/x2sql.exe src/x2sql.js
 
 
 
